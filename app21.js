1fef243699c76b4d3475f0d1fb441f53b24b40f3
Bahadur001 <161586965+Bahadur001@users.noreply.github.com>
2024-04-03 11:08:55 +0400

    commit

---
+++ app21.js
@@ -0,0 +1,64 @@
+// TASK 1
+// let students = [
+//     {
+//         id: 1,
+//         Name: "BMW",
+//         Point: 60000
+//     },
+//     {
+//         id: 2,
+//         Name: "Mercedes",
+//         Point: 80000
+//     },
+//     {
+//         id: 3,
+//         Name: "Bugatti",
+//         Point: 150000
+//     },
+//     {
+//         id: 4,
+//         Name: "Ferrary",
+//         Point: 95000
+//     },
+//     {
+//         id: 5,
+//         Name: "Cadillac",
+//         Point: 123000
+//     }
+// ];
+
+// students.sort((a, b) => a.Point - b.Point);
+// console.log(students.map(student => student.Name));
+
+// let random = [];
+// for (let i = 0; i < 5; i++) {
+//     random.push(Math.floor(Math.random() * 10));
+// }
+
+
+// console.log("Oluşturulan rastgele rakamlar:", random);
+
+// let sum = random.reduce((acc, curr) => acc + curr, 0);
+
+
+// console.log("Rastgele rakamların toplamı:", sum);
+
+
+// let random = Array.from({length: 5}, () => Math.floor(Math.random() * 10));
+
+// console.log("Oluşturulan rastgele rakamlar:", random);
+
+// let sum = random.reduce((acc, curr) => acc + curr, 0);
+
+// console.log("Rastgele rakamların toplamı:", sum);
+
+// TASK2
+
+// let random = [];
+// for (let i = 0; i < 5; i++) {
+//     let eded = Math.floor(Math.random() * 10);
+//     random[i] = eded;
+// }
+// console.log("random ededler", random);
+// let cemi = random.reduce((a, b) => a + b, 0);
+// console.log("cemi:", cemi);
